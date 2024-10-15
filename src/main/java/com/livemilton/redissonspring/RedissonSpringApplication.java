b00b3ed819ccf9fe09ee9c3cade530436655c8c8
Milton Muñoz <livemilton@gmail.com>
2024-10-14 22:51:44 -0500

    city service - controller - client - dto

--- src/main/java/com/livemilton/redissonspring/RedissonSpringApplication.java
+++ src/main/java/com/livemilton/redissonspring/RedissonSpringApplication.java
@@ -3,11 +3,13 @@ package com.livemilton.redissonspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan(basePackages = "com.livemilton.redissonspring.fib.config.RedissonCacheConfig.class")
 public class RedissonSpringApplication {
 
 	public static void main(String[] args) {
