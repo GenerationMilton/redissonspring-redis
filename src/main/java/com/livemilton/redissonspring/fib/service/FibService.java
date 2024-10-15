f81939009755263a1a9f3e5165e96f4503a938e2
Milton Muñoz <livemilton@gmail.com>
2024-10-14 20:41:22 -0500

    Scheduled cache evit

--- src/main/java/com/livemilton/redissonspring/fib/service/FibService.java
+++ src/main/java/com/livemilton/redissonspring/fib/service/FibService.java
@@ -2,6 +2,7 @@ package com.livemilton.redissonspring.fib.service;
 
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +18,12 @@ public class FibService {
     @CacheEvict(value = "math:fib", key = "#index")
     public void clearCache(int index){
         System.out.println("clearing hash key");
+    }
+
+    @Scheduled(fixedRate = 10_000)
+    @CacheEvict(value = "math:fib", allEntries = true)
+    public void clearCache(){
+        System.out.println("clearing all fib keys");
     }
 
     //intentional 2N
